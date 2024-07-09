d800b91e9fb5d6ac855c50ce86aa9b4a8b6fd4ff
Noir-0409 <146914778+Noir-0409@users.noreply.github.com>
2024-07-09 11:18:16 +0900

    関数作成まで

--- main.cpp
+++ main.cpp
@@ -806,7 +806,7 @@ D3D12_CPU_DESCRIPTOR_HANDLE GetCPUDescriptorHandle(ID3D12DescriptorHeap* descrip
 
 }
 
-D3D12_GPU_DESCRIPTOR_HANDLE GETGPUDescriptorHandle(ID3D12DescriptorHeap* descriptorHeap, uint32_t descriptorSize, uint32_t index) {
+D3D12_GPU_DESCRIPTOR_HANDLE GetGPUDescriptorHandle(ID3D12DescriptorHeap* descriptorHeap, uint32_t descriptorSize, uint32_t index) {
 
 	D3D12_GPU_DESCRIPTOR_HANDLE handleGPU = descriptorHeap->GetGPUDescriptorHandleForHeapStart();
 
@@ -1041,15 +1041,6 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 
 #pragma endregion
 
-	// 2枚目のTextureを読んで転送
-	DirectX::ScratchImage mipImage2 = LoadTexture("resources/monsterBall.png");
-
-	const DirectX::TexMetadata& metadata2 = mipImage2.GetMetadata();
-
-	ID3D12Resource* textureResource2 = CreateTextureResource(device, metadata2);
-
-	UploadTextureData(textureResource2, mipImage2);
-
 	// Textureを読んで転送する
 	DirectX::ScratchImage mipImages = LoadTexture("resources/UVChecker.png");
 
@@ -1060,16 +1051,6 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 	UploadTextureData(textureResource, mipImages);
 
 	// metaDataを基にSRVの設定
-	D3D12_SHADER_RESOURCE_VIEW_DESC srvDesc2{};
-
-	srvDesc2.Format = metadata2.format;
-
-	srvDesc2.Shader4ComponentMapping = D3D12_DEFAULT_SHADER_4_COMPONENT_MAPPING;
-
-	srvDesc2.ViewDimension = D3D12_SRV_DIMENSION_TEXTURE2D; // 2Dテクスチャ
-
-	srvDesc2.Texture2D.MipLevels = UINT(metadata2.mipLevels);
-
 	D3D12_SHADER_RESOURCE_VIEW_DESC srvDesc{};
 
 	srvDesc.Format = metadata.format;
@@ -1084,19 +1065,10 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 
 	ID3D12DescriptorHeap* srvDescriptorHeap = CreateDescriptorHeap(device, D3D12_DESCRIPTOR_HEAP_TYPE_CBV_SRV_UAV, 128, true);
 
-	// DescriptorSizeを取得しておく
-	const uint32_t descriptorSizeSRV = device->GetDescriptorHandleIncrementSize(D3D12_DESCRIPTOR_HEAP_TYPE_CBV_SRV_UAV);
-
-	const uint32_t descriptorSizeRTV = device->GetDescriptorHandleIncrementSize(D3D12_DESCRIPTOR_HEAP_TYPE_RTV);
-
-	const uint32_t descriptorSizeDSV = device->GetDescriptorHandleIncrementSize(D3D12_DESCRIPTOR_HEAP_TYPE_DSV);
-
-	GetCPUDescriptorHandle(rtvDescriptorHeap, descriptorSizeRTV, 0);
-
 	// SRVを作成するDescriptorHeapの場所を決める
-	D3D12_CPU_DESCRIPTOR_HANDLE textureSrvHandleCPU = GetCPUDescriptorHandle(rtvDescriptorHeap, descriptorSizeRTV, 0);
+	D3D12_CPU_DESCRIPTOR_HANDLE textureSrvHandleCPU = srvDescriptorHeap->GetCPUDescriptorHandleForHeapStart();
 
-	D3D12_GPU_DESCRIPTOR_HANDLE textureSrvHandleGPU = GETGPUDescriptorHandle(rtvDescriptorHeap, descriptorSizeRTV, 0);
+	D3D12_GPU_DESCRIPTOR_HANDLE textureSrvHandleGPU = srvDescriptorHeap->GetGPUDescriptorHandleForHeapStart();
 
 	// 先頭はimGuiが使っているのでその次を使う
 	textureSrvHandleCPU.ptr += device->GetDescriptorHandleIncrementSize(D3D12_DESCRIPTOR_HEAP_TYPE_CBV_SRV_UAV);
@@ -1428,7 +1400,7 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 
 			float v = 1.0f - float(latIndex) / float(kSubdivision);
 
-			 // 頂点データを入力。基準点a
+			// 頂点データを入力。基準点a
 			vertexData[start].position.x = cos(lat) * cos(lon);
 			vertexData[start].position.y = sin(lat);
 			vertexData[start].position.z = cos(lat) * sin(lon);
