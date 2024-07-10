b1e532145ab7dff31377d3dc561a8a97eb7c4c50
Noir-0409 <146914778+Noir-0409@users.noreply.github.com>
2024-07-10 13:13:38 +0900

    法線の定義追加まで

--- Object3d.VS.hlsl
+++ Object3d.VS.hlsl
@@ -12,6 +12,7 @@ struct VertexShaderInput {
 
 	float4 position : POSITION0;
 	float2 texcoord : TEXCOORD0;
+	float3 normal : NORMAL;
 
 };
 
--- main.cpp
+++ main.cpp
@@ -61,6 +61,7 @@ struct VertexData {
 
 	Vector4 position;
 	Vector2 texcoord;
+	Vector3 normal;
 
 };
 
@@ -1270,7 +1271,7 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 
 	assert(SUCCEEDED(hr));
 
-	D3D12_INPUT_ELEMENT_DESC inputElementDescs[2] = {};
+	D3D12_INPUT_ELEMENT_DESC inputElementDescs[3] = {};
 
 	inputElementDescs[0].SemanticName = "POSITION";
 
@@ -1287,6 +1288,14 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 	inputElementDescs[1].Format = DXGI_FORMAT_R32G32_FLOAT;
 
 	inputElementDescs[1].AlignedByteOffset = D3D12_APPEND_ALIGNED_ELEMENT;
+
+	inputElementDescs[2].SemanticName = "NORMAL";
+
+	inputElementDescs[2].SemanticIndex = 0;
+
+	inputElementDescs[2].Format = DXGI_FORMAT_R32G32B32_FLOAT;
+
+	inputElementDescs[2].AlignedByteOffset = D3D12_APPEND_ALIGNED_ELEMENT;
 
 	D3D12_INPUT_LAYOUT_DESC inputLayOutDesc{};
 
