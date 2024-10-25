1d6d0d61c3c84e5557358b0d550cbbe445026a1e
Noir-0409 <146914778+Noir-0409@users.noreply.github.com>
2024-10-25 11:42:15 +0900

    訂正

--- Particle.VS.hlsl
+++ Particle.VS.hlsl
@@ -23,6 +23,7 @@ struct VertexShaderInput
 VertexShaderOutput main(VertexShaderInput input,uint32_t instanceId:SV_InstanceID)
 {
 
+
 	VertexShaderOutput output;
 
 	output.position = mul(input.position, gTransformationMatrices[instanceId].WVP);
