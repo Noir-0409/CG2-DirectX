ff7bcd92535e423de2b8ce799c42ef427194d70b
Noir-0409 <146914778+Noir-0409@users.noreply.github.com>
2024-10-28 16:45:51 +0900

    終了

--- main.cpp
+++ main.cpp
@@ -270,31 +270,6 @@ IDxcBlob* CompileShader(
 	return shaderBlob;
 
 }
-
-/*LRESULT CALLBACK WindowProc(HWND hwnd, UINT msg, WPARAM wparam, LPARAM lparam) {
-
-	if (ImGui_ImplWin32_WndProcHandler(hwnd, msg, wparam, lparam)) {
-
-		return true;
-
-	}
-
-	switch (msg) {
-
-	case WM_DESTROY:
-
-		PostQuitMessage(0);
-
-		return 0;
-
-	}
-
-	return DefWindowProc(hwnd, msg, wparam, lparam);
-
-}*/
-
-/*ID3D12Resource* CreateBufferResource(ID3D12Device* device, size_t size)*/
-
 Microsoft::WRL::ComPtr<ID3D12Resource> CreateBufferResource(Microsoft::WRL::ComPtr<ID3D12Device> device,size_t size)
 {
 
@@ -1069,41 +1044,6 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 
 #pragma region Windowの生成
 
-	/*WNDCLASS wc{};
-
-	wc.lpfnWndProc = WindowProc;
-
-	wc.lpszClassName = L"CG2WindowClass";
-
-	wc.hInstance = GetModuleHandle(nullptr);
-
-	wc.hCursor = LoadCursor(nullptr, IDC_ARROW);
-
-	RegisterClass(&wc);
-
-	const int32_t kClientWidth = 1280;
-	const int32_t kClientHeight = 720;
-
-	RECT wrc = { 0,0,kClientWidth,kClientHeight };
-
-	AdjustWindowRect(&wrc, WS_OVERLAPPEDWINDOW, false);
-
-	HWND hwnd = CreateWindow(
-		wc.lpszClassName,
-		L"CG2",
-		WS_OVERLAPPEDWINDOW,
-		CW_USEDEFAULT,
-		CW_USEDEFAULT,
-		wrc.right - wrc.left,
-		wrc.bottom - wrc.top,
-		nullptr,
-		nullptr,
-		wc.hInstance,
-		nullptr);
-
-	ShowWindow(hwnd, SW_SHOW);*/
-
-	
 
 #pragma endregion
 
