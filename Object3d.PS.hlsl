cdaa63ce1cc1b534278b354f577136a4b7afcab7
Noir-0409 <146914778+Noir-0409@users.noreply.github.com>
2025-01-17 12:58:53 +0900

    最後まで、別のエラーでる

--- Object3d.PS.hlsl
+++ Object3d.PS.hlsl
@@ -46,22 +46,23 @@ PixelShaderOutput main(VertexShaderOutput input) {
 
     float3 toEye = normalize(gCamera.worldPosition - input.worldPosition);
     float3 reflectLight = reflect(gDirectionalLight.direction, normalize(input.normal));
-	
-    float RdotE = dot(reflectLight, toEye);
-    float specualarPow = pow(saturate(RdotE), gMaterial.shininess);
+
 	
 	if (gMaterial.enableLighting != 0) {
 
 		float NdotL = dot(normalize(input.normal), -gDirectionalLight.direction);
 		float cos = pow(NdotL * 0.5f + 0.5f, 2.0f);
 
+        float RdotE = dot(reflectLight, toEye);
+        float specualarPow = pow(saturate(RdotE), gMaterial.shininess);
+		
 		output.color = gMaterial.color * textureColor * gDirectionalLight.color * cos * gDirectionalLight.intensity;
 		
 		//拡散反射
         float3 diffuse = gMaterial.color.rgb * textureColor.rgb * gDirectionalLight.color.rgb * cos * gDirectionalLight.intensity;
 	
 	//鏡面反射
-        float3 specular = gDirectionalLight.color.rgb * gDirectionalLight.intensity * specular * float3(1.0f, 1.0f, 1.0f);
+        float3 specular = gDirectionalLight.color.rgb * gDirectionalLight.intensity * specualarPow * float3(1.0f, 1.0f, 1.0f);
 	
 	//拡散反射+鏡面反射
         output.color.rgb = diffuse + specular;
--- main.cpp
+++ main.cpp
@@ -1220,7 +1220,7 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 
 	descriptorRange[0].OffsetInDescriptorsFromTableStart = D3D12_DESCRIPTOR_RANGE_OFFSET_APPEND; // offsetを指導計算
 
-	D3D12_ROOT_PARAMETER rootParameters[4] = {};
+	D3D12_ROOT_PARAMETER rootParameters[5] = {};
 
 	rootParameters[0].ParameterType = D3D12_ROOT_PARAMETER_TYPE_CBV;
 	rootParameters[0].ShaderVisibility = D3D12_SHADER_VISIBILITY_PIXEL;
